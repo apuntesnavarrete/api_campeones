7d9a8cc030ba07c0be0c81c58eef9f50787c3c14
apuntesnavarrete <apuntesnavarrete@gmail.com>
2023-07-04 15:39:37 -0600

    estructura inicial

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({
+    "hola": "texto",
+    goles:"3"
+
+  });
+});
+
+module.exports = router;
